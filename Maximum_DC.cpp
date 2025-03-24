e3e74fbc54b2c2741ff405624cd92a9669b27eb8
Shivam-Kharbanda <shivamk290705@gmail.com>
2025-03-24 23:00:22 +0530

    Divide and Conquer

---
+++ Maximum_DC.cpp
@@ -0,0 +1,32 @@
+// To find max and min element from an array using divivde and conquer method
+
+#include <iostream>
+using namespace std;
+
+pair<int, int> findMaxMin(int arr[], int s, int e) {
+    if (s == e) 
+        return make_pair(arr[s], arr[s]);
+
+    int mid = s + (e - s) / 2;
+
+    pair<int, int> leftResult = findMaxMin(arr, s, mid);
+    pair<int, int> rightResult = findMaxMin(arr, mid + 1, e);
+
+    int maxi = max(leftResult.first, rightResult.first);
+    int mini = min(leftResult.second, rightResult.second);
+
+    return make_pair(maxi, mini);
+}
+
+int main() {
+    int arr[] = {12, 7, 19, 5, 18, 3, 8, 11, 22, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    pair<int, int> result = findMaxMin(arr, 0, n - 1);
+    
+    cout << "Maximum element: " << result.first << endl;
+    cout << "Minimum element: " << result.second << endl;
+
+    return 0;
+}
+
